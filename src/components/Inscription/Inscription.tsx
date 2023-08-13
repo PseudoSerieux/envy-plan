cf7a944c40bbe281db56d54e2b3ffa0fc4799ccf
Margaux <margauxretro@gmail.com>
2023-08-13 10:12:38 +0200

    :wrench: password control inscription

--- src/components/Inscription/Inscription.tsx
+++ src/components/Inscription/Inscription.tsx
@@ -1,14 +1,25 @@
-import React, { FormEvent, useRef, useState, useEffect } from 'react';
+import React, { FormEvent, useState, useEffect, useCallback } from 'react';
+import { useForm  } from 'react-hook-form';
+import * as yup from 'yup';
 import './Inscription.css'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Button, Checkbox, Container, Divider, FormControl, FormControlLabel, FormHelperText, Grid, useFormControl, InputLabel } from '@mui/material';
-import { theme } from '../../utils/theme';
+import { Button, Checkbox, Container, Divider, FormControl, FormControlLabel, FormHelperText, Grid, useFormControl } from '@mui/material';
 import { toast } from 'react-toastify';
 import { Link } from 'react-router-dom';
 import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
 
 const Inscription = () => {
+  const form = useForm({
+    reValidateMode: 'onBlur',
+  });
+
+  const schema = yup.object({
+    pseudo:yup.string().required(),
+    email:yup.string().email().required(),
+    password: yup.string().required().length(8).uppercase().lowercase(),
+  });
+
   //Controle Email
   const [email, setEmail] = useState('');
   const [error, setError] = useState('');
@@ -29,6 +40,24 @@ const Inscription = () => {
     setError(email.includes('@') || !email.endsWith('.com') || !email.endsWith('.fr') || !email.endsWith('.net') ? 'Email invalide' : '');
   }, [email]);
 
+  //Controle Pwd
+  const [password, setPassword] = useState('');
+  const [errorPwd, setErrorPwd] = useState('');
+  
+  const handleChangePwd = (e: React.ChangeEvent<HTMLInputElement>) => {
+    form.setValue('password', e.target.value);
+  };
+
+  const handleBlurPwd = useCallback(async () => {
+    const errors = await schema.validate(form.getValues);
+    const passwordError = errors.password;
+    setErrorPwd(passwordError);
+  }, [form.getValues, schema]);
+
+  useEffect(() => {
+    handleBlurPwd();
+  }, [form.formState, handleBlurPwd]);
+
   //helper down pswd
   function MyFormHelperText() {
     const { focused } = useFormControl() || {};
@@ -40,22 +69,18 @@ const Inscription = () => {
   
       return 'Helper text';
     }, [focused]);
-    return <FormHelperText>{helperText}</FormHelperText>;
+    return (<FormHelperText>{helperText}</FormHelperText>);
   }
 
   //Submit
   function handleSubmit(event: FormEvent<HTMLFormElement>): void {
+    // submitForm(form.getValues());
     throw new Error('Function not implemented.');
   }
 
     return (
         <Container>
-          <Grid container direction="column" justifyContent="center" alignItems="center">
-            <Box className="title" sx={{ display: 'flex', alignItems: 'center', p:2}}>
-            <h1> Bienvenue sur Envy Plan !</h1>
-            </Box>
-          </Grid>
-          <Box className="sub" component="form" onSubmit={handleSubmit} noValidate autoComplete="off">
+          <Box className="sub" component="form" onSubmit={handleSubmit} autoComplete="off">
             <Grid container direction="column" justifyContent="center" alignItems="center">
               <Box sx={{ display: 'flex', alignItems: 'center', p:2, fontSize: '1.3rem' }}>
 
@@ -63,7 +88,7 @@ const Inscription = () => {
               {/* une mémoire qui ne vous fait jamais défaut ? */}
               </Box>
 
-              {/* TODO : formControl */}
+              {/* TODO : formControl : MyFormHelper + Conf mdp*/}
               <FormControl>
               <Grid item p={1} display="flex" justifyContent="center" alignItems="center">
                 <TextField required label="Pseudo" variant="outlined"  
@@ -74,8 +99,9 @@ const Inscription = () => {
                            onBlur={handleBlur} onChange={handleChange}/>
               </Grid>
               <Grid item p={1} display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+                {/* Mdp requis : au moins 8 caractères + une lettre minuscule + une lettre majuscule */}
                 <TextField required label="Mot de passe" type="password"
-                           autoComplete="current-password" />
+                           autoComplete="current-password" onChange={handleChangePwd} onBlur={handleBlurPwd}/>
                            <MyFormHelperText />
                            {/* cf : useFormControl MUI */}
               </Grid>
@@ -90,7 +116,8 @@ const Inscription = () => {
 
             </Grid>
           
-          <Grid item p={2} display="flex" justifyContent="center" alignItems="center">            
+          <Grid item p={2} display="flex" justifyContent="center" alignItems="center">    
+                 {/* TODO : redirect connexion avec wait + toastr "Done : bien inscrits" */}
             <Link to="/inscription">
               <Button size="large" className="btn-insc" type="submit" variant="contained"
               sx={{ borderRadius: '1.25rem', textTransform: 'capitalize', px: 6, fontWeight: 'bold'}} color="primary">
